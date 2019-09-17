21bdd17dd922afbfafe709316781ee7017eb938a
mchedlish <mchedlish@yahoo.com>
2019-09-18 01:41:16 +0400

    google keys added

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
-const nodemon = require('nodemon');
+passport.use(new GoogleStrategy({
+clientID:keys.googleClientID,
+clientSecret:keys.googleClientSecret,
+callbackURL:'/auth/google/callback'
+}, accessToken=>{console.log(accessToken)}));
 
-app.get('/', (req, res)=>{
+app.get('/auth/google', passport.authenticate(
+'google', {
+scope: ['profile', 'email']
 
-    res.send({ნიკო: 'there'});
-});
+}
+
+))
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 console.log('Server STARTED')
